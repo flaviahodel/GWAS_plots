bc9a7c8f9d113a0e6a1b58726567b9d71fae1448
flaviahodel <flavia.hodel@epfl.ch>
2020-08-11 11:28:49 +0200

    Update ChicagoPlot.R

--- ChicagoPlot.R
+++ ChicagoPlot.R
@@ -1,7 +1,6 @@
 chicago_plot <- function(df1, df2, threshold) {
   
-  requireNamespace('ggplot2')
-  message(sprintf("Plotting %d points...", nrow(data)))
+  message(sprintf("%d SNPs from df1", nrow(df1)))
   
   plot_data_1 <- df1 %>%   
     # Compute chromosome size
@@ -15,6 +14,8 @@ chicago_plot <- function(df1, df2, threshold) {
     # Add a cumulative position of each SNP
     arrange(CHR, BP) %>%
     mutate( BPcum=as.numeric(BP+tot))
+  
+  message(sprintf("%d SNPs from df2", nrow(df2)))
   
   plot_data_2 <- df2 %>%   
     # Compute chromosome size
@@ -33,7 +34,7 @@ chicago_plot <- function(df1, df2, threshold) {
   plot_data_1$P <- -log10(plot_data_1$P)
   plot_data_2$P <- log10(plot_data_2$P)
   
-  ### 3. Top SNP  ###
+  ### Top SNP
   tmp_1 <- plot_data_1[plot_data_1$P > -log10(threshold), ]
   lead_snp_df_1 <- tmp_1[order(tmp_1$P, decreasing = TRUE), ]
   lead_snp_1 <- lead_snp_df_1[1,]
@@ -45,15 +46,15 @@ chicago_plot <- function(df1, df2, threshold) {
   # Bind
   plot_data <- rbind(plot_data_1, plot_data_2)
   
-  ### 2. Generate x-axis ###
+  ### Generate x-axis ###
   axisdf <- plot_data %>% 
     group_by(CHR) %>% 
     summarize(center=(max(BPcum) + min(BPcum)) / 2 )
   
-  ### 4. Create plot ###
-  ggplot(plot_data, aes(x=BPcum, y=P)) + 
+  ### Create plot ###
+  plot <- ggplot(plot_data, aes(x=BPcum, y=P)) + 
     #specify the y and x values
-    geom_point( aes(color=as.factor(CHR)), alpha = 0.8, size = 1.3) + 
+    geom_point(aes(color=as.factor(CHR)), alpha = 0.8, size = 1.3) + 
     # create scatterplot colored by chromosome
     scale_color_manual(values = rep(c("#4E79A7", "#A0CBE8"), 22)) + 
     # set a colour pattern 
@@ -61,15 +62,15 @@ chicago_plot <- function(df1, df2, threshold) {
     # scale the x-axis
     scale_y_continuous(limits = c(-max(abs(plot_data$P)+2), max(abs(plot_data$P)+2)), expand = expansion(mult = 0, add = 0)) + 
     # remove space between plot area and x axis
-    theme_publication() + 
     theme_light() +
     theme(legend.position="none",
           axis.text.x = element_text(size = rel(0.75)),
           plot.margin = margin(6,6,-2,6),
           panel.border = element_blank(),
-          panel.grid.major.x = element_blank(),
-          panel.grid.minor.x = element_blank(),
-          axis.line = element_line(color = "black")) +
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          axis.line = element_line(color = "black"),
+          panel.background  = element_blank()) + #ok until here
     # add x label
     xlab("Chromosome") + 
     # add y label
@@ -89,23 +90,11 @@ chicago_plot <- function(df1, df2, threshold) {
                      nudge_y = 2,
                      segment.size = 0.5,
                      vjust = 2,
-                     hjust = 1.5) 
-  geom_point(data = lead_snp_1, 
-             color = "red", size = 2) + # Add highlighted points 
-    geom_point(data = lead_snp_2, # add annotation value
-               color = "red", size = 2) + # Add highlighted points 
-    geom_hline(yintercept = c(0, -log10(threshold), log10(threshold)), linetype = "dashed") 
-
-  return(plot) # return the final plot
+                     hjust = 1.5) +
+    geom_point(data = lead_snp_1, color = "red", size = 2) + # Add highlighted points 
+    geom_point(data = lead_snp_2, color = "red", size = 2) + # Add highlighted points 
+    geom_hline(yintercept = c(0, -log10(threshold), log10(threshold)), linetype = c("solid", "dashed", "dashed")
+  
+  # return the final plot
+  return(plot) 
 }
-
-
-# Theme publication 
-theme_publication <- function(base_size = 16, base_family = "") {
-  requireNamespace('ggplot2')
-  thm = theme_bw(base_size = base_size, base_family = base_family)
-  thm + theme(axis.line = element_line(),
-              panel.border = element_blank(),
-              panel.background  = element_blank(),
-              panel.grid.major  = element_blank(),
-              panel.grid.minor  = element_blank())}
